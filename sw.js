0f9aa74170b39f2557be010f15333e9a58e5e65f
userv1925-cloud <userv1925@gmail.com>
2026-04-13 16:33:58 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,22 @@
-const CACHE_NAME = 'norma-v1';
-const assets = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'norma-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
 });
